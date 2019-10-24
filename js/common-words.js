2712e66fdf4959055fccc9264e1b2f6c26ba267a
John Carbajal <johnxjuanito@gmail.com>
2019-10-24 08:55:55 -0700

    work word

---
+++ js/common-words.js
@@ -0,0 +1,159 @@
+// Here are the 100 most popular words in English, as totally
+// stolen from here: https://gist.github.com/gravitymonkey/2406023
+
+// var commonWords = [
+//   "the",
+//   "of",
+//   "and",
+//   "a",
+//   "to",
+//   "in",
+//   "is",
+//   "you",
+//   "that",
+//   "it",
+//   "he",
+//   "was",
+//   "for",
+//   "on",
+//   "are",
+//   "as",
+//   "with",
+//   "his",
+//   "they",
+//   "I",
+//   "at",
+//   "be",
+//   "this",
+//   "have",
+//   "from",
+//   "or",
+//   "one",
+//   "had",
+//   "by",
+//   "word",
+//   "but",
+//   "not",
+//   "what",
+//   "all",
+//   "were",
+//   "we",
+//   "when",
+//   "your",
+//   "can",
+//   "said",
+//   "there",
+//   "use",
+//   "an",
+//   "each",
+//   "which",
+//   "she",
+//   "do",
+//   "how",
+//   "their",
+//   "if",
+//   "will",
+//   "up",
+//   "other",
+//   "about",
+//   "out",
+//   "many",
+//   "then",
+//   "them",
+//   "these",
+//   "so",
+//   "some",
+//   "her",
+//   "would",
+//   "make",
+//   "like",
+//   "him",
+//   "into",
+//   "time",
+//   "has",
+//   "look",
+//   "two",
+//   "more",
+//   "write",
+//   "go",
+//   "see",
+//   "number",
+//   "no",
+//   "way",
+//   "could",
+//   "people",
+//   "my",
+//   "than",
+//   "first",
+//   "water",
+//   "been",
+//   "call",
+//   "who",
+//   "oil",
+//   "its",
+//   "now",
+//   "find",
+//   "long",
+//   "down",
+//   "day",
+//   "did",
+//   "get",
+//   "come",
+//   "made",
+//   "may",
+//   "part"
+// ]
+
+// function pickWord() {
+//   var word = commonWords[Math.floor(Math.random() * commonWords.length)]
+//   var wordSpaces = ""
+//   // const under = commonWords.map(l => '_')
+
+//   for (var i = word.length - 1; i >= 0; i--) wordSpaces += "_ "
+
+//   document.getElementById("spaces").innerHTML = wordSpaces
+// }
+
+// function searchLetter() {
+//   //Get Value
+//   //Search word arrray
+//   //if true change spaces array
+//   //else change images
+// }
+
+// // $(document).ready(function()){
+
+// // const word = 'flowers'
+// // const under = word.map(l => '_')
+
+// // console.log(word, under)
+
+// // let alphabet ='abcdefghijkmnopqrstuvwxyz'
+// // let buttons = ''
+// // alphabet.split("").foreach(letter => {
+// //   buttons += `<buttons>${letter.toUpperCase()}</button>`
+// //   var guess = $(this))
+// //   .html()
+// //   .tolowercase()
+// //    $(this).attr("disable"), true)
+
+// // if (local var)  under:string[]{
+// //   under = under.map((u,i) => {
+// //     if (u === '_'){
+
+// //     }
+// //     if (words [i] === guess){
+// //       return words[i]
+// //     }else{
+// //       return "_"
+// //     }
+// //   } else {
+// //     return u
+// //   }
+// //   })
+
+// //  $("#underscores").html(under.join(" "))
+// // }else{
+// //   console.log("bad guess")
+// //  })
+// // })
